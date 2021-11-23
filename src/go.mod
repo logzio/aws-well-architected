a61de5e2da286ab66c31593cd287d3678f3a57c9
ShiranAvidov <shiran.avidov@logz.io>
2021-11-23 10:48:32 +0200

    init

---
+++ src/go.mod
@@ -0,0 +1,32 @@
+module AwsWellArchetectured
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.27.0
+	github.com/aws/aws-sdk-go-v2/config v1.10.2
+	github.com/aws/aws-sdk-go-v2/service/wellarchitected v1.8.1
+	github.com/aws/smithy-go v1.9.0
+	github.com/logzio/logzio-go v1.0.3
+)
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/aws/aws-sdk-go-v2 v1.11.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.6.2 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.0.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.5.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.6.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.10.1 // indirect
+	github.com/beeker1121/goque v2.1.0+incompatible // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/shirou/gopsutil/v3 v3.21.6 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,283 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/wellarchitected"
+	"github.com/aws/aws-sdk-go-v2/service/wellarchitected/types"
+	"github.com/aws/smithy-go/logging"
+	"github.com/logzio/logzio-go"
+)
+
+type WellArchitected struct {
+	ctx    context.Context
+	logger logging.Logger
+	client *wellarchitected.Client
+	data   [][]byte
+}
+
+const (
+	maxBulkSizeBytes = 10 * 1024 * 1024 // 10 MB
+)
+
+func NewWellArchitected(ctx context.Context) (*WellArchitected, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("error loading default config: %v", err)
+	}
+
+	client := wellarchitected.NewFromConfig(cfg)
+
+	if err != nil {
+		return nil, fmt.Errorf("error creating LogzioSender: %v", err)
+	}
+
+	return &WellArchitected{
+		ctx:    	  ctx,
+		logger: 	  cfg.Logger,
+		client: 	  client,
+	}, nil
+}
+
+func HandleRequest(ctx context.Context) error {
+	wellArchitected, err := NewWellArchitected(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	wellArchitected.logger.Logf("INFO", "Collecting data...")
+
+	err = wellArchitected.collectData()
+
+	if err != nil {
+		return err
+	}
+
+	wellArchitected.logger.Logf("INFO", "Sending data to Logz.io...")
+
+	err = wellArchitected.sendDataToLogzio()
+
+	if err != nil {
+		return err
+	}
+
+	wellArchitected.logger.Logf("INFO", "Finished successfully")
+
+	return nil
+}
+
+func (wa *WellArchitected) collectData() error {
+	workloadSummaries, err := wa.getWorkloadSummaries()
+
+	if err != nil {
+		return err
+	}
+
+	for _, workloadSummary := range workloadSummaries {
+		workloadID := workloadSummary.WorkloadId
+		workload, err := wa.getWorkload(workloadID)
+
+		if err != nil {
+			return err
+		}
+
+		for _, lens := range workload.Lenses {
+			lensReview, err := wa.getLensReview(workloadID, &lens)
+
+			if err != nil {
+				return err
+			}
+
+			pillarReviewSummaries := lensReview.LensReview.PillarReviewSummaries
+
+			for _, pillarReviewSummary := range pillarReviewSummaries {
+				err := wa.parseLensReview(lensReview, &pillarReviewSummary)
+
+				if err != nil {
+					return err
+				}
+			}
+
+			lensReviewImprovements, err := wa.getLensReviewImprovements(workloadID, &lens)
+
+			if err != nil {
+				return err
+			}
+
+			improvementSummaries := lensReviewImprovements.ImprovementSummaries
+
+			for _, improvementSummary := range improvementSummaries {
+				err := wa.parseLensReviewImprovements(lensReviewImprovements, &improvementSummary)
+
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (wa *WellArchitected) getWorkloadSummaries() ([]types.WorkloadSummary, error) {
+	listWorkloadsInput := &wellarchitected.ListWorkloadsInput{}
+	listWorkloadsOutput, err := wa.client.ListWorkloads(wa.ctx, listWorkloadsInput)
+
+	if err != nil {
+		return nil, fmt.Errorf("did not get workload summaries: %v", err)
+	}
+
+	return listWorkloadsOutput.WorkloadSummaries, nil
+}
+
+func (wa *WellArchitected) getWorkload(workloadID *string) (*types.Workload, error) {
+	workloadInput := &wellarchitected.GetWorkloadInput{WorkloadId: workloadID}
+	workloadOutput, err := wa.client.GetWorkload(wa.ctx, workloadInput)
+
+	if err != nil {
+		return nil, fmt.Errorf("did not get workload with id %s: %v", *workloadID, err)
+	}
+
+	workloadJSON, err := json.Marshal(workloadOutput.Workload)
+
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling workload with id %s: %v", *workloadID, err)
+	}
+
+	wa.data = append(wa.data, workloadJSON)
+
+	return workloadOutput.Workload, nil
+}
+
+func (wa *WellArchitected) getLensReview(workloadID *string, lensAlias *string) (*wellarchitected.GetLensReviewOutput, error) {
+	lensReviewInput := &wellarchitected.GetLensReviewInput{
+		WorkloadId: workloadID,
+		LensAlias:  lensAlias,
+	}
+
+	lensReviewOutput, err := wa.client.GetLensReview(wa.ctx, lensReviewInput)
+
+	if err != nil {
+		return nil, fmt.Errorf("did not get lens review with workload id %s and lens alias %s: %v", *workloadID, *lensAlias, err)
+	}
+
+	return lensReviewOutput, nil
+}
+
+func (wa *WellArchitected) parseLensReview(lensReview *wellarchitected.GetLensReviewOutput, pillarReviewSummary *types.PillarReviewSummary) error {
+	lensReviewJSON, err := json.Marshal(lensReview)
+
+	if err != nil {
+		return fmt.Errorf("error marshaling lens review with workload id %s and lens alias %s: %v",
+			*lensReview.WorkloadId, *lensReview.LensReview.LensAlias, err)
+	}
+
+	var lensReviewMap map[string]interface{}
+	err = json.Unmarshal(lensReviewJSON, &lensReviewMap)
+
+	if err != nil {
+		return fmt.Errorf("error unmarshaling lens review with workload id %s and lens alias %s: %v",
+			*lensReview.WorkloadId, *lensReview.LensReview.LensAlias, err)
+	}
+
+	delete(lensReviewMap["LensReview"].(map[string]interface{}), "PillarReviewSummaries")
+
+	lensReviewMap["LensReview"].(map[string]interface{})["PillarReviewSummary"] = pillarReviewSummary
+
+	parsedLensReviewJSON, err := json.Marshal(lensReviewMap)
+
+	if err != nil {
+		return fmt.Errorf("error marshaling lens review with workload id %s and lens alias %s: %v",
+			*lensReview.WorkloadId, *lensReview.LensReview.LensAlias, err)
+	}
+
+	wa.data = append(wa.data, parsedLensReviewJSON)
+
+	return nil
+}
+
+func (wa *WellArchitected) getLensReviewImprovements(workloadID *string, lensAlias *string) (*wellarchitected.ListLensReviewImprovementsOutput, error) {
+	lensReviewImprovementsInput := &wellarchitected.ListLensReviewImprovementsInput{
+		WorkloadId: workloadID,
+		LensAlias:  lensAlias,
+	}
+
+	lensReviewImprovementsOutput, err := wa.client.ListLensReviewImprovements(wa.ctx, lensReviewImprovementsInput)
+
+	if err != nil {
+		return nil, fmt.Errorf("did not get lens review improvements with workload id %s and lens alias %s: %v", *workloadID, *lensAlias, err)
+	}
+
+	return lensReviewImprovementsOutput, nil
+}
+
+func (wa *WellArchitected) parseLensReviewImprovements(
+	lensReviewImprovements *wellarchitected.ListLensReviewImprovementsOutput,
+	improvementSummary *types.ImprovementSummary,
+) error {
+	lensReviewImprovementsJSON, err := json.Marshal(lensReviewImprovements)
+
+	if err != nil {
+		return fmt.Errorf("error marshaling lens review improvements with workload id %s and lens alias %s: %v",
+			*lensReviewImprovements.WorkloadId, *lensReviewImprovements.LensAlias, err)
+	}
+
+	var lensReviewImprovementsMap map[string]interface{}
+	err = json.Unmarshal(lensReviewImprovementsJSON, &lensReviewImprovementsMap)
+
+	if err != nil {
+		return fmt.Errorf("error unmarshaling lens review improvements with workload id %s and lens alias %s: %v",
+			*lensReviewImprovements.WorkloadId, *lensReviewImprovements.LensAlias, err)
+	}
+
+	delete(lensReviewImprovementsMap, "ImprovementSummaries")
+
+	lensReviewImprovementsMap["ImprovementSummary"] = improvementSummary
+
+	parsedLensReviewJSON, err := json.Marshal(lensReviewImprovementsMap)
+
+	if err != nil {
+		return fmt.Errorf("error marshaling lens review improvements with workload id %s and lens alias %s: %v",
+			*lensReviewImprovements.WorkloadId, *lensReviewImprovements.LensAlias, err)
+	}
+
+	wa.data = append(wa.data, parsedLensReviewJSON)
+
+	return nil
+}
+
+func (wa *WellArchitected) sendDataToLogzio() error {
+	logzioSender, err := logzio.New(
+		os.Getenv("LOGZIO_TOKEN"),
+		logzio.SetUrl(os.Getenv("LOGZIO_URL")),
+		logzio.SetInMemoryQueue(true),
+		logzio.SetinMemoryCapacity(maxBulkSizeBytes),
+	)
+
+	if err != nil {
+		return fmt.Errorf("error creating LogzioSender: %v", err)
+	}
+
+	for _, data := range wa.data {
+		err := logzioSender.Send(data)
+
+		if err != nil {
+			return fmt.Errorf("error sending data to Logz.io: %v", err)
+		}
+	}
+
+	logzioSender.Stop()
+
+	return nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
